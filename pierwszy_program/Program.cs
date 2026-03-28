14ce0126b2d03fa82fb9746ded39b2d3dc4f8567
Luke <s939040@student.chorzow.merito.pl>
2026-03-28 21:01:17 +0100

    Aktualizacja plików z wykładu

--- pierwszy_program/Program.cs
+++ pierwszy_program/Program.cs
@@ -2,9 +2,17 @@
 {
     private static void Main(string[] args)
     {
+        Podprogram1();
+        Console.WriteLine(4+4);
+        Console.WriteLine("4"+"4");
+    }
+
+    static void Podprogram1()
+    {
+        string szkola = "Szkola Podstawowa";
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Jak Masz na imię?");
         string ?imie = Console.ReadLine();
-        Console.WriteLine($"Cześć buraku, {imie}!");
+        Console.WriteLine($"Cześć buraku, {imie}, nie zdajesz {szkola}");
     }
 }
