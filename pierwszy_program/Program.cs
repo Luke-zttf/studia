bdbada805765434a0841d9730d5b33cd7d6954c6
Luke <s939040@student.chorzow.merito.pl>
2026-03-28 20:13:11 +0100

    new advanced tool/program

---
+++ pierwszy_program/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Jak Masz na imię?");
+        string ?imie = Console.ReadLine();
+        Console.WriteLine($"Cześć buraku, {imie}!");
+    }
+}
